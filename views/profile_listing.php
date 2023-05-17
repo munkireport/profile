0649237760189ae0ac8ac9cdae5d8ca81a101406
tuxudo <tuxudo@users.noreply.github.com>
2023-05-17 17:59:20 -0400

    v6 style

--- views/profile_listing.php
+++ views/profile_listing.php
@@ -1,11 +1,9 @@
 <?php $this->view('partials/head'); ?>
 
-<div class="container">
-  <div class="row">
+<div class="container-fluid">
+  <div class="row pt-4">
 	<div class="col-lg-12">
-
-	  <h3><span data-i18n="profile.report"></span> <span id="total-count" class='label label-primary'>…</span></h3>
-
+	  <h3><span data-i18n="profile.report"></span> <span id="total-count" class='badge badge-primary'>…</span></h3>
 	  <table class="table table-striped table-condensed table-bordered">
 		<thead>
 		  <tr>
@@ -24,7 +22,6 @@
             <th data-i18n="profile.profile_description" data-colname='profile.profile_description'></th>
 		  </tr>
 		</thead>
-
 		<tbody>
 		  <tr>
 			<td data-i18n="listing.loading" colspan="13" class="dataTables_empty"></td>
